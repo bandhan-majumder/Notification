a17d092480efffe0a44e7f765a11856e3236908e
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-03-15 17:47:41 +0530

    dynamic port for frontend

--- Novu/frontend/vite.config.ts
+++ Novu/frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
+import dotenv from 'dotenv'
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT ?? '') || 5432, // Default to 3000 if PORT is not set
+    host: '0.0.0.0' // Ensure it's accessible from external networks
+  }
 })
